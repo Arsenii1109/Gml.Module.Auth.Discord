dd1a0eb523ef07f59009316a5e90e5e02990bd8a
akemiko <tcop29711@gmail.com>
2024-08-14 09:18:05 +0300

    Initialize Gml.Auth.Api and Gml.Service.Discord projects
    Add initial project setup including .NET 8.0, configuration files, and Docker support for Gml.Auth.Api and Gml.Service.Discord. Includes basic API endpoint and background service with logging.

---
+++ src/Gml.Service.Discord/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Gml.Service.Discord;
+
+public class DiscordService : BackgroundService
+{
+    private readonly ILogger<DiscordService> _logger;
+
+    public DiscordService(ILogger<DiscordService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
